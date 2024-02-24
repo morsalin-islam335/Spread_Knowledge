b1ff2e3df1a14030d607446957c83189bf602e72
morsalin-islam335 <weshallovercome335@gmail.com>
2024-02-24 18:50:11 +0600

    conformation link

--- account/views.py
+++ account/views.py
@@ -63,7 +63,7 @@ def registration(request):
 
             token = default_token_generator.make_token(user) # user er jonno token generate korba
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            conform_link = f"https://spread-knowledge-2/account/activate/{uid}/{token}"
+            conform_link = f"https://spread-knowledge/account/activate/{uid}/{token}.onerender.com"
             # conform_link = f"http://127.0.0.1:8000/account/activate/{uid}/{token}"
 
 
